491ba6a6bacc255c03829309c79dfd33752536d4
lucas adriel <lucasadriel007@gmail.com>
2023-11-28 10:31:34 -0400

    estilizaçao do componente feita

--- src/app/pages/home/cadastro-funcionarios/cadastro-funcionarios.component.ts
+++ src/app/pages/home/cadastro-funcionarios/cadastro-funcionarios.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro-funcionarios',
   templateUrl: './cadastro-funcionarios.component.html',
   styleUrls: ['./cadastro-funcionarios.component.scss']
 })
-export class CadastroFuncionariosComponent {
-
+export class CadastroFuncionariosComponent implements OnInit {
+  public count: number = 0;
+  public elements = document.getElementsByClassName("content")
+  constructor() {}
+  ngOnInit(): void {
+    var inputElement = this.elements[0] as HTMLElement
+    inputElement.focus()
+  }
+  public alterFocus() {
+    this.count = this.count + 1
+    switch(this.count) {
+      case 0:
+        var inputElement = this.elements[0] as HTMLElement
+        inputElement.focus()
+        break
+      case 1:
+        var inputElement = this.elements[1] as HTMLElement
+        inputElement.focus()
+        break
+    } 
+  }
 }
