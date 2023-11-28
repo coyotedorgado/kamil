cf2280ce83c3c24123634593c4b9d6ef4a6e743f
lucas adriel <lucasadriel007@gmail.com>
2023-11-28 15:12:26 -0400

    atalho de tecla enter nos inputs

--- src/app/pages/home/cadastro-funcionarios/cadastro-funcionarios.component.ts
+++ src/app/pages/home/cadastro-funcionarios/cadastro-funcionarios.component.ts
@@ -13,6 +13,16 @@ export class CadastroFuncionariosComponent implements OnInit {
     var inputElement = this.elements[0] as HTMLElement
     inputElement.focus()
   }
+  //zerar o contador para evitar bug de chamar o caso 3 do swith
+  public zerar() {
+    this.count = 0
+  }
+  public zerar1() {
+    this.count = 1
+  }
+  public zerar2() {
+    this.count = 2
+  }
   public alterFocus() {
     this.count = this.count + 1
     switch(this.count) {
@@ -24,6 +34,14 @@ export class CadastroFuncionariosComponent implements OnInit {
         var inputElement = this.elements[1] as HTMLElement
         inputElement.focus()
         break
-    } 
+      case 2:
+        var inputElement = this.elements[2] as HTMLElement
+        inputElement.focus()
+        break
+      case 3:
+        var inputElement = this.elements[3] as HTMLElement
+        inputElement.focus()
+        break
+    }
   }
 }
