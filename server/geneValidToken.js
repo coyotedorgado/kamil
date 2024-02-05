ce857fd53df904a18d45f5ae736823889bec4937
lucas adriel <lucasadriel007@gmail.com>
2024-02-05 15:16:28 -0400

    gerar token de validaçao

---
+++ server/geneValidToken.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+const secretKey = '145863256325896258962589625896254896214785456655959+'
+
+function gerarToken(usuario) {
+    const token = jwt.sign({usuario}, secretKey, { expiresIn: '6h' })
+    return token
+}
+function validarToken(token) {
+    try{
+        const decoded = jwt.verify(token, secretKey)
+        return decoded.usuario
+    }catch (error){
+        throw new Error('chave invalida')
+    }
+}
+
+module.exports = { gerarToken, validarToken }
