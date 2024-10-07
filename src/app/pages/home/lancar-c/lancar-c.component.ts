8c53436fcaca874bfde122a91d1585f3c6c3f874
lucas adriel <lucasadriel007@gmail.com>
2024-10-07 16:32:00 -0400

    passos finais da estilizaçao do lançador de comandas

--- src/app/pages/home/lancar-c/lancar-c.component.ts
+++ src/app/pages/home/lancar-c/lancar-c.component.ts
@@ -8,6 +8,7 @@ import { AfterViewInit, Component } from '@angular/core';
 export class LancarCComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     this.alternar_cor();
+    //chama a funcao de inserir os clientes no data list do html
     this.inserirClientes();
   }
   public async inserirClientes(){
@@ -15,11 +16,13 @@ export class LancarCComponent implements AfterViewInit {
     var requisicao = await fetch("http://localhost:3000/clientes");
     var clientes = await requisicao.json()
     for(let i = 0; i < clientes.length; i++){
+      //cria o elemento option atribue o value(nome do cliente) e  o inserir no datalist
       const option = document.createElement("option");
       option.value = clientes[i].nome;
       lista_clients?.appendChild(option);
     }
   }
+  // funçao estilizadora do fundo que troca a troca toda vez que mover o mouse
   public alternar_cor(){
     var el = document.getElementById("body")!;
     var cor1 = Math.floor(Math.random() * 255);
